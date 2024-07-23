f0adc211382fc7f2b9a79d25b6faeda769797a9f
Pham Tien Dat <tdat9663@gmail.com>
2024-07-23 18:34:16 +0700

    cap nhat gio hang

--- Routes/client/cart.route.js
+++ Routes/client/cart.route.js
@@ -1,6 +1,9 @@
 const express= require('express');
 const route=express.Router()
 const controller=require('../../controllers/client/cart.controller');
-route.post('/add/:productId',controller.addPost)
 
+route.get('/',controller.index)
+route.post('/add/:productId',controller.addPost)
+route.get('/delete/:productId',controller.delete)
+route.get('/update/:productId/:quantity',controller.update)
 module.exports=route
--- public/js/script.js
+++ public/js/script.js
@@ -14,3 +14,18 @@ if (listButtonPagination.length > 0) {
 }
 // end pagination
 
+// cập nhật số lượng sản phẩm trong giỏ hàng
+const quantityList= document.querySelectorAll("[cart] input[name='quantity']")
+if(quantityList.length>0){
+  quantityList.forEach(item=>{
+    item.addEventListener('change',()=>{
+      const productId=item.getAttribute('item-id')
+      const quantity=item.value
+      if(productId && quantity>0)
+        window.location.href=`/cart/update/${productId}/${quantity}`
+    })
+  })
+}
+
+
+//hết cập nhật số lượng sản phẩm trong giỏ hàng
