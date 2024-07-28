79b6e34f81bb6f3501e826559e7d731f7859dfd6
Pham Tien Dat <tdat9663@gmail.com>
2024-07-28 22:18:37 +0700

    3 đổi mk thành công

--- Routes/client/user.route.js
+++ Routes/client/user.route.js
@@ -3,12 +3,20 @@ const route= express.Router()
 const controller= require('../../controllers/client/user.controller');
 route.get( `/register`,controller.register)
 route.post(`/register`,controller.registerPost)
+
 route.get('/login',controller.login)
 route.post('/login',controller.lgoinPost)
+
 route.get('/logout',controller.logout)
+
 route.get('/password/forgot',controller.forgot)
 route.post('/password/forgot',controller.forgotPost)
 
+route.get('/password/otp',controller.otpPassword)
+route.post('/password/otp',controller.otpPasswordPost)
+
+route.get('/password/reset',controller.resetPassword)
+route.patch('/password/reset',controller.resetPasswordPatch)
 
 
 
